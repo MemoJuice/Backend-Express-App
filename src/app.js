499fabb946c834d09e8f79ff49e5736f28d847ad
MemoJuice <Wrpxdouble1135@gmail.com>
2025-12-29 11:47:16 +0700

    API CRUD Succes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { router as apiRoutes } from "./routes/index.js";
+import cors from "cors";
 
 export const app = express();
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
