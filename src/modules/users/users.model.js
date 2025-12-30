ad67ea0eddbcbf57bc4ebe8999f087ddb90be4d2
MemoJuice <Wrpxdouble1135@gmail.com>
2025-12-30 15:47:08 +0700

    edit Hash password

--- src/modules/users/users.model.js
+++ src/modules/users/users.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
 // Hash paswword before saving
 userSchema.pre("save", async function(){
  if(!this.isModified("password")) return
- this.password = await bcrypt.hash(this.password, 10)ว
+ this.password = await bcrypt.hash(this.password, 10);
 });
 
 // mongoDB will automatically create users collection
