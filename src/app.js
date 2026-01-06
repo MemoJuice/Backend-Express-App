a81fd4d46645e3597fa151b702e3f7a9fb1f6aae
MemoJuice <Wrpxdouble1135@gmail.com>
2026-01-06 15:35:54 +0700

    backend update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import express from "express";
-import { router as apiRoutes } from "./routes/index.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { router as apiRoutes } from "./routes/index.js";
 
 export const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:5173",
-            "https://frontend-app-seven-xi.vercel.app"]
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "https://jsd-react-assessment-solution.vercel.app",
+  ],
+  credentials: true, // ✅  allow cookies to be sent
 };
-  credential: true, // 🟩 allow cookie to be sent
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-// Middleware to parse cookie (required for cookie-base auth)
+// Middleware to parse cookies (required for cookie-based auth)
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
@@ -27,20 +31,20 @@ app.use("/api", apiRoutes);
 // Catch-all for 404 Not Found
 app.use((req, res, next) => {
   const error = new Error(`Not found: ${req.method} ${req.originalUrl}`);
-  error.name = "NotFoundError";
-  error.status = 404;
+  error.name = error.name || "NotFoundError";
+  error.status = error.status || 404;
   next(error);
 });
 
-// Centralized Error Handling Middelware
-app.use((err, req, res, next ) => {
- console.error(err.stack);
- res.status(err.status || 500).json({
+// Centralized Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
     success: false,
-    message: err.massage || "Internal Server Error",
+    message: err.message || "Internal Server Error",
     path: req.originalUrl,
     method: req.method,
     timestamp: new Date().toISOString(),
     stack: err.stack,
- });
+  });
 });
